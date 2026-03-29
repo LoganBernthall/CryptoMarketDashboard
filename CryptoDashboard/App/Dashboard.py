ac889d848fcf82688e3d2e588686bf8536a4c2a1
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2026-03-29 18:33:07 +0100

    Building historical data request

--- CryptoDashboard/App/Dashboard.py
+++ CryptoDashboard/App/Dashboard.py
@@ -1,8 +1,10 @@
 #Crypto Dashboard webapp
-
+import datetime
 import streamlit as st
 import requests
+import pandas as pd
 
+#Titles
 st.title("Crypto Market Dashboard")
 st.caption("Welcome, this app utilise the CoinParpirka Crypto API")
 
@@ -14,21 +16,38 @@ cryptoMap = {
     "BNB": "bnb-binance-coin"
 }
 
-
+#User select crypro from list
 selectedCrypto = st.selectbox("Choose a cryptocurrency", list(cryptoMap.keys()))
 
 # Get API ticker
 ticker = cryptoMap[selectedCrypto]
-# Call API
+
+# Call API for current price
 url = f"https://api.coinpaprika.com/v1/tickers/{ticker}"
 response = requests.get(url)
-
 price = response.json()["quotes"]["USD"]["price"]
-
 st.subheader("Price:")
 st.metric(label=selectedCrypto, value=f"${price:,.2f}")
 
-st.subheader("Historical:")
-urlHistorical = f"https://api.coinpaprika.com/v1/tickers/{ticker}/historical?end=NOW&limit=1000&quote=usd&interval=5m"
-historicalPrice = response.json()
-st.metric(label=selectedCrypto, value=f"${historicalPrice:,.2f}")
+## Call API for historical
+st.subheader(f"Historical 7 Days of {selectedCrypto}")
+
+endTime = datetime.datetime.utcnow()
+startTime = endTime - datetime.timedelta(days=7)  # last 7 days
+
+urlHistorical = f"https://api.coinpaprika.com/v1/tickers/{ticker}/historical?start={startTime.isoformat()}Z&end={endTime.isoformat()}Z&quote=usd&interval=5m"
+
+responseHistorical = requests.get(urlHistorical)
+
+historicalPrice = responseHistorical.json()
+
+# Parse into df and format if possible
+HistoricalDF = pd.DataFrame()  # empty df by default
+if isinstance(historicalPrice, list) and len(historicalPrice) > 0:
+    HistoricalDF = pd.DataFrame(historicalPrice)
+    HistoricalDF["timestamp"] = pd.to_datetime(HistoricalDF["timestamp"])
+    HistoricalDF.set_index("timestamp", inplace=True)
+    st.subheader(f"Historical {selectedCrypto}")
+    st.line_chart(HistoricalDF[["price"]])
+else:
+    st.warning("No historical data available for this coin/interval")
