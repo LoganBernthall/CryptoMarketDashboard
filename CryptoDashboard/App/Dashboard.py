cc7ff1123d2b504514c6dc73e56ec1f70f10f21f
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2026-03-30 16:59:05 +0100

    Update to Dahsboard
    *Fixed API call - CoinPaprika free has a 1 day delay on historical.
    *Added cardano, dodgecoin ticker
    *Planning to further build coin info to be called from API

--- CryptoDashboard/App/Dashboard.py
+++ CryptoDashboard/App/Dashboard.py
@@ -6,14 +6,17 @@ import pandas as pd
 
 #Titles
 st.title("Crypto Market Dashboard")
-st.caption("Welcome, this app utilise the CoinParpirka Crypto API")
+st.caption("Welcome, this app utilise the CoinParpirka Crypto API.")
+st.caption("This is using the free API so there is a 1 day delay on selected calls.")
 
 #Dictionary mapping of cryptos stored in box and what the request would require
 cryptoMap = {
     "Bitcoin": "btc-bitcoin",
     "Ethereum": "eth-ethereum",
     "Solana": "sol-solana",
-    "BNB": "bnb-binance-coin"
+    "BNB": "bnb-binance-coin",
+    "Cardano": "ada-cardano",
+    "DogeCoin": "doge-dogecoin"
 }
 
 #User select crypro from list
@@ -30,16 +33,20 @@ st.subheader("Price:")
 st.metric(label=selectedCrypto, value=f"${price:,.2f}")
 
 ## Call API for historical
-st.subheader(f"Historical 7 Days of {selectedCrypto}")
-
 endTime = datetime.datetime.utcnow()
 startTime = endTime - datetime.timedelta(days=7)  # last 7 days
 
-urlHistorical = f"https://api.coinpaprika.com/v1/tickers/{ticker}/historical?start={startTime.isoformat()}Z&end={endTime.isoformat()}Z&quote=usd&interval=5m"
+#Formatting of time
+start_str = startTime.strftime("%Y-%m-%dT%H:%M:%SZ")
+end_str = endTime.strftime("%Y-%m-%dT%H:%M:%SZ")
 
+urlHistorical = f"https://api.coinpaprika.com/v1/tickers/{ticker}/historical?start={start_str}&end={end_str}&quote=usd&interval=1d"
 responseHistorical = requests.get(urlHistorical)
 
 historicalPrice = responseHistorical.json()
+
+#Debug for html response commented out
+#st.write(responseHistorical.text)
 
 # Parse into df and format if possible
 HistoricalDF = pd.DataFrame()  # empty df by default
@@ -47,7 +54,11 @@ if isinstance(historicalPrice, list) and len(historicalPrice) > 0:
     HistoricalDF = pd.DataFrame(historicalPrice)
     HistoricalDF["timestamp"] = pd.to_datetime(HistoricalDF["timestamp"])
     HistoricalDF.set_index("timestamp", inplace=True)
-    st.subheader(f"Historical {selectedCrypto}")
+    st.subheader(f"7 Day Historical {selectedCrypto} (1 Day Delay)")
     st.line_chart(HistoricalDF[["price"]])
 else:
     st.warning("No historical data available for this coin/interval")
+
+#Call for information from the coin 
+st.subheader(f"{selectedCrypto} Info:")
+
